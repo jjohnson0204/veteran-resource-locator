5c89780e8c4ef549332e5ba5d2569136e323eb41
jjohnson0204 <joejohnson0204@gmail.com>
2026-05-23 01:14:49 -0400

    chore: initial project setup with Spring Boot, JPA, Security, and Docker

---
+++ src/main/java/com/veteranresources/veteranresourcelocator/VeteranResourceLocatorApplication.java
@@ -0,0 +1,13 @@
+package com.veteranresources.veteranresourcelocator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeteranResourceLocatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VeteranResourceLocatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/veteranresources/veteranresourcelocator/VeteranResourceLocatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veteranresources.veteranresourcelocator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeteranResourceLocatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
